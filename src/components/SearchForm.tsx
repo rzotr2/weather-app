1508131a8e1617fca5a041c967e83f61e6e032c3
rzotr2 <pavel.lukin126@gmail.com>
2025-05-29 18:24:36 +0200

    QUICK FIX - paddings and margins;

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -95,7 +95,7 @@ export function SearchForm({getWeatherDataAndCity}: SearchFormProps) {
                             bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 cursor-pointer
                             focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800
                             dark:text-gray-200 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                        Find by city
+                        {t("submitButton")}
                     </button>
                 </form>
             </div>
